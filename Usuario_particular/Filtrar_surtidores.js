62e4b59f57838a072579724649ff01eaa2790c9e
JhessaelGit <siancas.jhessael@gmail.com>
2026-03-21 20:52:45 -0400

    refactor: Se saco la funcion filtrar fuera del evento

--- Usuario_particular/Filtrar_surtidores.js
+++ Usuario_particular/Filtrar_surtidores.js
@@ -1,24 +1,24 @@
 const boton_filtro = document.querySelector('#button-filtrar')
 const surtidores = document.querySelectorAll('.surtidor');
 const opcion_filtrado= document.querySelector("#opcion-filtrado")
+function filtrar(valor){
+    if(valor === 'todos')
+    {    
+        surtidores.forEach(surtidor => {
+            surtidor.style.display = ''; 
+        });
+    }
+    else if(valor === 'disponibles'){
+        surtidores.forEach(surtidor => {
+            const estado = surtidor.querySelector('.estado-actual');
+            if (estado && estado.classList.contains('agotado')) {
+                surtidor.style.display = 'none'; 
+            }
+        });
+    }
+}
 
 boton_filtro.addEventListener('click',() =>{
     const opcion_filtrado_value = opcion_filtrado.value; 
-    function filtrar(valor){
-        if(valor === 'todos')
-        {    
-            surtidores.forEach(surtidor => {
-                surtidor.style.display = ''; 
-            });
-        }
-        else if(valor === 'disponibles'){
-            surtidores.forEach(surtidor => {
-                const estado = surtidor.querySelector('.estado-actual');
-                if (estado && estado.classList.contains('agotado')) {
-                    surtidor.style.display = 'none'; 
-                }
-            });
-        }
-    }
     filtrar(opcion_filtrado_value);
 })
